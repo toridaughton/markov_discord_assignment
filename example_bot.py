ff01c427ea1b48399512fbf7dde291a8ba69a36c
Tori Daughton <toriddaughton@gmail.com>
2022-05-27 06:55:33 -0600

    Completed assignment

---
+++ example_bot.py
@@ -0,0 +1,103 @@
+import os
+import discord
+from random import choice
+
+def open_and_read_file(file_path):
+    """Take file path as string; return text as string.
+
+    Takes a string that is a file path, opens the file, and turns
+    the file's contents as one string of text.
+    """
+    file = open(file_path)
+    contents = file.read()
+    file.close()
+
+    return contents
+
+
+def make_chains(text_string):
+    """Take input text as string; return dictionary of Markov chains.
+
+    A chain will be a key that consists of a tuple of (word1, word2)
+    and the value would be a list of the word(s) that follow those two
+    words in the input text.
+
+    For example:
+
+        >>> chains = make_chains('hi there mary hi there juanita')
+
+    Each bigram (except the last) will be a key in chains:
+
+        >>> sorted(chains.keys())
+        [('hi', 'there'), ('mary', 'hi'), ('there', 'mary')]
+
+    Each item in chains is a list of all possible following words:
+
+        >>> chains[('hi', 'there')]
+        ['mary', 'juanita']
+
+        >>> chains[('there','juanita')]
+        [None]
+    """
+
+    chains = {}
+
+    words = text_string.split()
+
+    words.append(None)
+
+    for i in range(len(words) - 2):
+        key = (words[i], words[i + 1])
+        value = words[i + 2]
+        if key not in chains:
+           chains[key] = []
+        chains[key].append(value)
+    return chains
+
+
+def make_text(chains):
+    """Return text from chains."""
+
+    key = choice(list(chains.keys()))
+    words = [key[0], key[1]]
+    word = choice(chains[key])
+
+    while word is not None:
+        key = (key[1], word)
+        words.append(word)
+        word = choice(chains[key])
+        
+
+    return ' '.join(words)
+
+
+input_path = 'green-eggs.txt'
+
+# Open the file and turn it into one long string
+input_text = open_and_read_file(input_path)
+
+# Get a Markov chain
+chains = make_chains(input_text)
+
+# Produce random text
+random_text = make_text(chains)
+
+
+
+client = discord.Client()
+
+@client.event
+
+async def on_ready():
+    print(f'Connected successfully! Logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('hello'):
+        await message.channel.send('Hello!')
+    else: 
+        await message.channel.send(random_text)
+
+client.run(os.environ['DISCORD_TOKEN'])
